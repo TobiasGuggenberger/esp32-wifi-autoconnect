1c7477f6bb14208c7f051f3fc7db950f2ba32fd2
Guggenberger <github2022@guggenberger.me>
2022-07-09 19:56:31 +0200

    init

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <time.h>
+#include <AutoConnect.h>
+
+
+WebServer Server;
+
+AutoConnect       Portal(Server);
+AutoConnectConfig Config;       // Enable autoReconnect supported on v0.9.4
+
+void rootPage() {
+  String  content =
+    "<html>"
+    "<head>"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+    "</head>"
+    "<body>"
+    "<h2 align=\"center\" style=\"color:blue;margin:20px;\">Hello, world</h2>"
+    "<h3 align=\"center\" style=\"color:gray;margin:10px;\">{{DateTime}}</h3>"
+    "<p style=\"text-align:center;\">Reload the page to update the time.</p>"
+    "<p></p><p style=\"padding-top:15px;text-align:center\">" AUTOCONNECT_LINK(COG_24) "</p>"
+    "</body>"
+    "</html>";
+  Server.send(200, "text/html", content);
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  Serial.println();
+
+
+  // Enable saved past credential by autoReconnect option,
+  // even once it is disconnected.
+  Config.autoReconnect = true;
+  Config.hostName = "esp32-01";
+  Portal.config(Config);
+
+
+  // Behavior a root path of ESP8266WebServer.
+  Server.on("/", rootPage);
+
+  Serial.println("Creating portal and trying to connect...");
+  // Establish a connection with an autoReconnect option.
+  if (Portal.begin()) {
+    Serial.println("WiFi connected: " + WiFi.localIP().toString());
+    Serial.println(WiFi.getHostname());
+
+  }
+
+}
+
+void loop() {
+  Portal.handleClient();
+}
